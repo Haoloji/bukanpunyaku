9a308d4ac3dd48588b8db33afa3bc73c10ee302b
child <112723082+Haoloji@users.noreply.github.com>
2023-04-14 02:34:16 +0700

    Update config.js
    Signed-off-by: child <112723082+Haoloji@users.noreply.github.com>

--- config.js
+++ config.js
@@ -83,7 +83,7 @@ global.lynk = '~_~'
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ MEDIA SOSSIAL ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 global.sig = '>_<'
-global.sgh = 'https://github.com/Haoloji' //github
+global.sgh = 'https://whatsapp.com/GPLdBQL9L293hSc9JrosJm' //github
 global.sgc = 'http://whatsapp.com/GPLdBQL9L293hSc9JrosJm' //group whatsapp
 global.sdc = '-' //discord
 global.snh = '≧∇≦'
